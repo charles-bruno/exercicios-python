521b30ef7497389a32ab6b236bb92cfa85d9fc22
Charles <charlesbruno046@gmail.com>
2022-01-09 20:10:12 +0000

    exercicios

---
+++ exercicio_livro_python.py
@@ -0,0 +1,19 @@
+# Converta as seguintes expresões  matemáticas para que possam ser calculasdas usando interpretador python.py
+#a = 10
+# b = 20
+#c = 30
+a = int(input('digite um valor '))
+b = int(input('digite um valor'))
+c = int(input('digite um valor '))
+s = a+b*c
+print('A soma entre {}  e outro numero {} e outro numero {} e igual {}.'.format(a,b,c,s))
+
+
+
+
+
+
+
+
+
+

01776ced491930179d9c16bdd990c6c39f48070a
Charles <charlesbruno046@gmail.com>
2022-01-09 20:20:10 +0000

    corrige exercicos

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ```
 git status
 git diff
-git add <nome do arquivo>
+git add nome-do-arquivo
 git commit -m "mensagem"
 git push origin master
 ```
--- exercicio_livro_python.py
+++ exercicio_livro_python.py
@@ -1,12 +1,12 @@
 # Converta as seguintes expresões  matemáticas para que possam ser calculasdas usando interpretador python.py
-#a = 10
+# a = 10
 # b = 20
-#c = 30
-a = int(input('digite um valor '))
-b = int(input('digite um valor'))
-c = int(input('digite um valor '))
+# c = 30
+a = int(input('digite um valor: '))
+b = int(input('digite um valor: '))
+c = int(input('digite um valor: '))
 s = a+b*c
-print('A soma entre {}  e outro numero {} e outro numero {} e igual {}.'.format(a,b,c,s))
+print('A soma entre {}  e outro numero {} e outro numero {} e igual {}.'.format(a, b, c, s))
 
 
 
